663111859f8f7746d31a26a17179e240a78e97d0
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-12-12 22:07:38 -0500

    complete lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog","cat","rodent","bird","reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner="None"):
+        self.name = name
+
+        if pet_type not in self.PET_TYPES:
+            raise Exception 
+        
+        self.pet_type = pet_type
+        self.owner=owner
+
+        self.all.append(self)
 
 class Owner:
-    pass
+    def __init__ (self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet,Pet):
+            raise Exception
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        pet_list = [pet for pet in Pet.all if pet.owner == self]
+        return sorted(pet_list, key=lambda pet:pet.name)
